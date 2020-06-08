83d529386d63965bbbf6e70e58dead0eec0d6133
Jakub Ciepły <jakubc01@gmail.com>
2020-06-08 22:15:27 +0200

    Updates

---
+++ precache-manifest.f390e79193a3230377ada9fddc435730.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12fc6e6f6b05895d09dac10124aa427",
+    "url": "/certificate-generator/index.html"
+  },
+  {
+    "revision": "a422aae83c4fe71569fb",
+    "url": "/certificate-generator/static/css/main.c2286ac3.chunk.css"
+  },
+  {
+    "revision": "f6ccd9b93a37152ed9c0",
+    "url": "/certificate-generator/static/js/2.5a1aeb05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/certificate-generator/static/js/2.5a1aeb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a422aae83c4fe71569fb",
+    "url": "/certificate-generator/static/js/main.d1e71fb2.chunk.js"
+  },
+  {
+    "revision": "629cf1e350b063528caf",
+    "url": "/certificate-generator/static/js/runtime-main.3be47103.js"
+  },
+  {
+    "revision": "32f856ad3b17ae0a9a6f598c06cb7b41",
+    "url": "/certificate-generator/static/media/sad.32f856ad.svg"
+  },
+  {
+    "revision": "1fc8f34a1020ec3b56d4a5a1e0da7917",
+    "url": "/certificate-generator/static/media/welcome.1fc8f34a.svg"
+  }
+]);
